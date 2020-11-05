6f22ab250a4c837403ccfa89e22c6a2a882effae
JasonGlazer <jglazer@gard.com>
2020-11-05 12:26:47 -0600

    Reorg code. Add list of objects and explanation

--- epjsoneditor/interface/frame.py
+++ epjsoneditor/interface/frame.py
@@ -1,24 +1,39 @@
 import wx
 import wx.lib.agw.aui as aui
+import os
+import json
+
+from epjsoneditor.schemainputobject import SchemaInputObject
 
 
 class EpJsonEditorFrame(wx.Frame):
 
     def __init__(self, parent, id=-1, title="epJSON Editor - ", pos=wx.DefaultPosition,
-               size=(800, 600), style=wx.DEFAULT_FRAME_STYLE):
+                 size=(1200, 800), style=wx.DEFAULT_FRAME_STYLE):
         wx.Frame.__init__(self, parent, id, title, pos, size, style)
 
         self._mgr = aui.AuiManager()
 
+        self.data_dictionary = {}
+        self.create_data_dictionary()
+
+        self.object_list_tree = None
+        self.object_list_root = None
+        self.create_gui()
+
+    def create_gui(self):
+
         # notify AUI which frame to use
         self._mgr.SetManagedWindow(self)
 
-        # create several text controls
-        text1 = wx.TextCtrl(self, -1, "Pane 1 - sample text",
-                            wx.DefaultPosition, wx.Size(200, 500),
-                            wx.NO_BORDER | wx.TE_MULTILINE)
+        self.object_list_tree = wx.TreeCtrl(self, style=wx.TR_HIDE_ROOT)
+        self.object_list_root = self.object_list_tree.AddRoot("All Input Objects")
+        self.object_list_tree.SetItemData(self.object_list_root, None)
+        for child in self.data_dictionary.keys():
+            child = self.object_list_tree.AppendItem(self.object_list_root, child)
+            self.object_list_tree.Expand(child)
 
-        text2 = wx.TextCtrl(self, -1, "Pane 2 - sample text",
+        self.explanation_text = wx.TextCtrl(self, -1, "Explanation of the selected input object and field",
                             wx.DefaultPosition, wx.Size(200, 150),
                             wx.NO_BORDER | wx.TE_MULTILINE)
 
@@ -32,17 +47,34 @@ class EpJsonEditorFrame(wx.Frame):
 
         # add the panes to the manager
         self._mgr.AddPane(text4, aui.AuiPaneInfo().CenterPane())
-        self._mgr.AddPane(text2, aui.AuiPaneInfo().Top().Caption("Explanation"))
+        self._mgr.AddPane(self.explanation_text, aui.AuiPaneInfo().Top().Caption("Explanation"))
         self._mgr.AddPane(text3, aui.AuiPaneInfo().Bottom().Caption("Search"))
         # Layer(2) allows it to take all of left side
-        self._mgr.AddPane(text1, aui.AuiPaneInfo().Left().Layer(2).Caption("List of Objects"))
+        self._mgr.AddPane(self.object_list_tree, aui.AuiPaneInfo().Left().Layer(2).Caption("List of Input Objects"))
 
         # tell the manager to "commit" all the changes just made
         self._mgr.Update()
 
         self.Bind(wx.EVT_CLOSE, self.OnClose)
+        self.Bind(wx.EVT_TREE_SEL_CHANGED, self.select_object_list_item, self.object_list_tree)
 
     def OnClose(self, event):
         # deinitialize the frame manager
         self._mgr.UnInit()
         event.Skip()
+
+    def select_object_list_item(self, event):
+        selected_object_name = self.object_list_tree.GetItemText(event.GetItem())
+        explanation = selected_object_name + os.linesep + os.linesep \
+                      + self.data_dictionary[selected_object_name].memo
+        self.explanation_text.Value = explanation
+
+    def create_data_dictionary(self):
+        """
+         Create the simplified version of the Energy+.schema.epJSON that
+         is closer to what is needed for displaying the grid elements
+        """
+        with open("c:/EnergyPlusV9-4-0/Energy+.schema.epJSON") as schema_file:
+            epschema = json.load(schema_file)
+            for object_name, json_properties in epschema["properties"].items():
+                self.data_dictionary[object_name] = SchemaInputObject(json_properties)
--- epjsoneditor/main.py
+++ epjsoneditor/main.py
@@ -1,23 +1,4 @@
-import json
-from epjsoneditor.schemainputobject import SchemaInputObject
 from epjsoneditor.interface.application import EpJsonEditorApplication
-
-data_dictionary = {}
-
-
-def create_data_dictionary():
-    """
-     Create the simplified version of the Energy+.schema.epJSON that
-     is closer to what is needed for displaying the grid elements
-    """
-    with open("c:/EnergyPlusV9-4-0/Energy+.schema.epJSON") as schema_file:
-        epschema = json.load(schema_file)
-        for object_name, json_properties in epschema["properties"].items():
-            data_dictionary[object_name] = SchemaInputObject(json_properties)
-
-# just for testing
-create_data_dictionary()
-print()
 
 app = EpJsonEditorApplication(0)
 app.MainLoop()
