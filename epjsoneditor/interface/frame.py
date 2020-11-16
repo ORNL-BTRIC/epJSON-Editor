015da316ca08b487d47716cb14b22324ed417c93
JasonGlazer <jglazer@gard.com>
2020-11-16 16:00:50 -0600

    Add toolbar

--- epjsoneditor/interface/frame.py
+++ epjsoneditor/interface/frame.py
@@ -54,7 +54,7 @@ class EpJsonEditorFrame(wx.Frame):
         self._mgr.AddPane(text4, aui.AuiPaneInfo().CenterPane().Name("text_content"))
         self._mgr.AddPane(self.explanation_text, aui.AuiPaneInfo().Top().Caption("Explanation"))
 
-        self.main_grid = self.CreateGrid()
+        self.main_grid = self.create_grid()
         self._mgr.AddPane(self.main_grid, aui.AuiPaneInfo().Name("grid_content").
                           CenterPane().Hide().MinimizeButton(True))
 
@@ -70,6 +70,8 @@ class EpJsonEditorFrame(wx.Frame):
         self._mgr.GetPane("text_content").Show(False)
         self._mgr.Update()
 
+        self.create_toolbar()
+
         self.Bind(wx.EVT_CLOSE, self.OnClose)
         self.Bind(wx.EVT_TREE_SEL_CHANGED, self.select_object_list_item, self.object_list_tree)
 
@@ -79,11 +81,56 @@ class EpJsonEditorFrame(wx.Frame):
         grid.CreateGrid(5, 5)
         return grid
 
-    def CreateGrid(self):
-        grid = wx.grid.Grid(self, -1, wx.Point(0, 0), wx.Size(150, 250),
-                            wx.NO_BORDER | wx.WANTS_CHARS)
-        grid.CreateGrid(50, 20)
-        return grid
+    def create_toolbar(self):
+        # create some toolbars
+        tb1 = aui.AuiToolBar(self, -1, wx.DefaultPosition, wx.DefaultSize,
+                             agwStyle=aui.AUI_TB_TEXT)
+        tb1.SetToolBitmapSize(wx.Size(48, 48))
+        tb1.AddSimpleTool(10, "New", wx.ArtProvider.GetBitmap(wx.ART_NEW))
+        tb_open_file = tb1.AddSimpleTool(11, "Open", wx.ArtProvider.GetBitmap(wx.ART_FILE_OPEN))
+        self.Bind(wx.EVT_TOOL, self.handle_open_file, tb_open_file)
+
+        tb1.AddSimpleTool(12, "Save", wx.ArtProvider.GetBitmap(wx.ART_FILE_SAVE))
+        tb1.AddSimpleTool(13, "Save As", wx.ArtProvider.GetBitmap(wx.ART_FILE_SAVE_AS))
+        tb1.AddSeparator()
+        tb1.AddSimpleTool(14, "Undo", wx.ArtProvider.GetBitmap(wx.ART_UNDO))
+        tb1.AddSimpleTool(15, "Redo", wx.ArtProvider.GetBitmap(wx.ART_REDO))
+        tb1.AddSeparator()
+        tb1.AddSimpleTool(16, "New Obj", wx.ArtProvider.GetBitmap(wx.ART_PLUS))
+        tb1.AddSimpleTool(17, "Dup Obj", wx.ArtProvider.GetBitmap(wx.ART_GO_FORWARD)) # duplicate
+        tb1.AddSimpleTool(18, "Dup Obj + Chg", wx.ArtProvider.GetBitmap(wx.ART_GOTO_LAST)) # duplicate and change
+        tb1.AddSimpleTool(19, "Del Obj", wx.ArtProvider.GetBitmap(wx.ART_MINUS))
+        tb1.AddSimpleTool(20, "Copy Obj", wx.ArtProvider.GetBitmap(wx.ART_COPY))
+        tb1.AddSimpleTool(21, "Paste Obj", wx.ArtProvider.GetBitmap(wx.ART_PASTE))
+        tb1.AddSeparator()
+        tb1.AddTool(22, "IP Units", wx.ArtProvider.GetBitmap(wx.ART_GO_UP), wx.NullBitmap, kind=wx.ITEM_RADIO)
+
+        tb1.AddTool(23, "SI Units", wx.ArtProvider.GetBitmap(wx.ART_GO_DOWN), wx.NullBitmap, kind=wx.ITEM_RADIO)
+        tb1.AddSeparator()
+        tb1.AddSimpleTool(25, "Settings", wx.ArtProvider.GetBitmap(wx.ART_EXECUTABLE_FILE))
+        tb1.AddSimpleTool(26, "Help", wx.ArtProvider.GetBitmap(wx.ART_HELP))
+        tb1.Realize()
+        self._mgr.AddPane(tb1, aui.AuiPaneInfo().Name("tb1").Caption("Primary Toolbar").
+                          ToolbarPane().Top())
+        self._mgr.Update()
+
+    def handle_open_file(self, event):
+        with wx.FileDialog(self, "Open EnergyPlus epJSON file", wildcard="epJSON files (*.epJSON)|*.epJSON",
+                           style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
+            if fileDialog.ShowModal() == wx.ID_CANCEL:
+                return     # the user changed their mind
+            path_name = fileDialog.GetPath()
+            try:
+               self.load_current_file(path_name)
+            except IOError:
+                wx.LogError(f"Cannot open file {path_name}")
+
+    def load_current_file(self, path_name):
+        if os.path.exists(path_name):
+            self.current_file = path_name
+            self.SetTitle(f"epJSON Editor - {path_name}")
+            self.Refresh()
+
 
     def OnClose(self, event):
         # deinitialize the frame manager
@@ -96,7 +143,6 @@ class EpJsonEditorFrame(wx.Frame):
                       + self.data_dictionary[selected_object_name].memo
         self.explanation_text.Value = explanation
         self.update_grid(selected_object_name)
-
 
     def update_grid(self, selected_object_name):
         self.main_grid.FreezeTo(0,1)
@@ -130,7 +176,7 @@ class EpJsonEditorFrame(wx.Frame):
             self.main_grid.SetCellValue(counter, 0, "")
             if "units" in current_field:
                 self.main_grid.SetCellValue(counter, 0, current_field["units"])
-            self.main_grid.SetCellBackgroundColour(counter, 0, wx.LIGHT_GREY)
+            #self.main_grid.SetCellBackgroundColour(counter, 0, wx.LIGHT_GREY)
 
         field_count = len(input_fields)
         for repeat in range(1, repeat_extension_fields + 1):
@@ -143,7 +189,7 @@ class EpJsonEditorFrame(wx.Frame):
                 self.main_grid.SetCellValue(counter + field_count, 0, "")
                 if "units" in current_field:
                     self.main_grid.SetCellValue(counter + field_count, 0, current_field["units"])
-                self.main_grid.SetCellBackgroundColour(counter + field_count, 0, wx.LIGHT_GREY)
+                #self.main_grid.SetCellBackgroundColour(counter + field_count, 0, wx.LIGHT_GREY)
             field_count += len(extension_fields)
         self.main_grid.SetRowLabelSize(300)
         self.main_grid.SetRowLabelAlignment(wx.ALIGN_LEFT,wx.ALIGN_TOP)
