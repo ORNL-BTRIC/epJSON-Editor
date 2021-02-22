f5c5b6b29ea8b19d5a024cbf9a970863777f8332
JasonGlazer <jglazer@gard.com>
2021-02-22 09:26:05 -0600

    Add help about and update license.

--- epjsoneditor/interface/frame.py
+++ epjsoneditor/interface/frame.py
@@ -172,7 +172,11 @@ class EpJsonEditorFrame(wx.Frame):
         tool_main.AddSeparator()
         tb_settings = tool_main.AddSimpleTool(25, "Settings", wx.ArtProvider.GetBitmap(wx.ART_EXECUTABLE_FILE))
         self.Bind(wx.EVT_TOOL, self.handle_settings_toolbar_button, tb_settings)
-        tool_main.AddSimpleTool(26, "Help", wx.ArtProvider.GetBitmap(wx.ART_HELP))
+        tb_help = tool_main.AddSimpleTool(26, "Help", wx.ArtProvider.GetBitmap(wx.ART_HELP))
+        tool_main.SetToolDropDown(26, True)
+        self.Bind(wx.EVT_TOOL, self.handle_tb_help_menu, tb_help)
+        #tb_help = tool_main.AddTool(26, "Help", wx.ArtProvider.GetBitmap(wx.ART_HELP),
+        #                            wx.ArtProvider.GetBitmap(wx.ART_HELP), kind=wx.ITEM_DROPDOWN)
         tool_main.Realize()
         self._mgr.AddPane(tool_main, aui.AuiPaneInfo().Name("tool_main").Caption("Primary Toolbar").
                           ToolbarPane().Top())
@@ -866,6 +870,51 @@ class EpJsonEditorFrame(wx.Frame):
             self.object_list_tree.SelectItem(object_list_item)  # this triggers update_grid
             self.update_grid(self.selected_object_name)
             self.update_list_of_object_counts()
+
+    def handle_tb_help_menu(self, event):
+        help_menu = wx.Menu()
+        tb_help_about = help_menu.Append(wx.NewId(), "About")
+        self.Bind(wx.EVT_MENU, self.handle_tb_help_about, tb_help_about)
+        self.PopupMenu(help_menu)
+
+    def handle_tb_help_about(self, event):
+        print('tb help about')
+        text = """
+epJSON Editor
+
+Version 0.1
+Copyright (c) 2021, Alliance for Sustainable Energy, LLC  and GARD Analytics, Inc
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+this list of conditions and the following disclaimer in the documentation
+and/or other materials provided with the distribution.
+
+* The name of the copyright holder(s), any contributors, the United States
+Government, the United States Department of Energy, or any of their employees
+may not be used to endorse or promote products derived from this software
+without specific prior written permission from the respective party.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER(S) AND ANY CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER(S), ANY ,
+CONTRIBUTORS THE UNITED STATES GOVERNMENT, OR THE UNITED STATES DEPARTMENT
+OF ENERGY, NOR ANY OF THEIR EMPLOYEES, BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        """
+        with wx.MessageDialog(self, text, 'About epJSON Editor', wx.OK | wx.ICON_INFORMATION) as dlg:
+            dlg.ShowModal()
 
     @staticmethod
     def is_convertible_to_float(value):
